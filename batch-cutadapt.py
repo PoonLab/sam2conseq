8c3257997d74948340089d15b7680ca20bcfe054
ArtPoon <artpoon@gmail.com>
2020-04-10 22:47:49 -0400

    batch cutadapt

--- batch-cutadapt.py
+++ batch-cutadapt.py
@@ -1,11 +1,13 @@
 from glob import glob
 import re
 import subprocess
+import sys
 
-files = glob('data/SRR*.fastq')
+files = glob('/data/covid/SRR*.fastq')
 
 # ignore FASTQ output from previous cutadapt runs
 files = [f for f in files if '.trim.' not in f]
+
 
 # determine sample names
 pat = re.compile("SRR[0-9]+")
@@ -13,18 +15,18 @@ samples = [pat.findall(f)[0] for f in files]
 samples = set(samples)
 
 for sample in samples:
-    if '{}_1.fastq'.format(sample) in files:
+    if '/data/covid/{}_1.fastq'.format(sample) in files:
         subprocess.check_call([
-            'cutadapt', '-q', '20,20', '-a', 'CTGTCTCTTATACACATCT', 
+            'cutadapt', '--cores', '6', '-q', '20,20', '-a', 'CTGTCTCTTATACACATCT', 
             '-A', 'CTGTCTCTTATACACATCT', 
-            '-o', 'data/{}_1.trim.fastq'.format(sample),
-            '-p', 'data/{}_2.trim.fastq'.format(sample),
-            'data/{}_1.fastq'.format(sample), 'data/{}_2.fastq'.format(sample)
+            '-o', '/data/covid/{}_1.trim.fastq'.format(sample),
+            '-p', '/data/covid/{}_2.trim.fastq'.format(sample),
+            '/data/covid/{}_1.fastq'.format(sample), '/data/covid/{}_2.fastq'.format(sample)
         ])
     else:
         subprocess.check_call([
-            'cutadapt', '-q', '20,20', '-a', 'CTGTCTCTTATACACATCT',  
-            '-o', 'data/{}.trim.fastq'.format(sample),
-            'data/{}.fastq'.format(sample)
+            'cutadapt', '--cores', '6', '-q', '20,20', '-a', 'CTGTCTCTTATACACATCT',  
+            '-o', '/data/covid/{}.trim.fastq'.format(sample),
+            '/data/covid/{}.fastq'.format(sample)
         ])
 
